1843d5d042d109f950c78c097470a22d02e89a4e
Ítalo Gomes <47856024+italoog@users.noreply.github.com>
2021-03-03 17:18:55 -0300

    1.0.3

--- src/services/CreateRuleService.ts
+++ src/services/CreateRuleService.ts
@@ -31,6 +31,27 @@ class CreateRuleService {
     // if (findRuleInSameDay) {
     //   throw Error('This rule is already saved');
     // }
+    if (daily && weekly) {
+      throw Error(
+        'Invalid rule. You cannot have a rule that is daily and weekly at the same time.',
+      );
+    }
+
+    if (daily && day) {
+      throw Error(
+        'Invalid rule. It is not possible to have a daily rule stating the day.',
+      );
+    }
+
+    if (weekly && day) {
+      throw Error(
+        'Invalid rule. It is not possible to have a weekly rule stating the day.',
+      );
+    }
+
+    if (!daily && !weekly && !day) {
+      throw Error('Invalid rule. A type of rule must be informed.');
+    }
 
     const rule = this.rulesRepository.create({
       daily,
